7b6f529224a7b817efd9698475d5eee3c7f320df
Felipe Fischer <ffsilva8@ucs.br>
2020-10-22 22:45:05 -0300

    first commit

---
+++ Library.WebService/Library.WebService/LibraryService.asmx
@@ -0,0 +1,1 @@
+﻿<%@ WebService Language="C#" CodeBehind="LibraryService.asmx.cs" Class="Library.WebService.LibraryService" %>
---
+++ LibraryClient/LibraryClient/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            localhost.LibraryService libraryService = new localhost.LibraryService();
+
+            Console.WriteLine(libraryService.HelloWorld());
+
+            Console.ReadKey();
+
+        }
+    }
+}
